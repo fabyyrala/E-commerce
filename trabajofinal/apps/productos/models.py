4e99e35d8e71ad36b69b6b6b0db8851f6eb8a0fb
fabyyrala <fabiyrala@gmail.com>
2024-06-10 15:16:51 -0300

    Categorias

--- trabajofinal/apps/productos/models.py
+++ trabajofinal/apps/productos/models.py
@@ -2,10 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return self.nombre
+    
+    
 class Producto(models.Model):
     nombre  = models.CharField(max_length=130)
     desc  = models.CharField(max_length=240)
-    categoria = models.CharField(max_length=30)
+    categoria = models.ManyToManyField(Categoria)
     precio = models.FloatField()
     activo  = models.BooleanField(default=True)
 
@@ -13,8 +20,9 @@ class Producto(models.Model):
         return f"{self.nombre}"
     
 
-
 class Favorito(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
     producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
     fecha_agregado = models.DateTimeField(auto_now_add=True)
+    
+    
